d9ba6cb61a2ef738502591c3e57c61bd87a6cf01
Masihur Maruf <marufrahman1349@gmail.com>
2017-11-19 22:14:55 -0500

    update the code

--- main/main.go
+++ main/main.go
@@ -14,7 +14,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 */
 func sum() int {
 	sum := 0
-	for i := 3; i <= 1000; i++ {
+	for i := 3; i < 1000; i++ {
 		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
